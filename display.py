844cb44d6a23858dc01cb48aa148a94d243b5969
Lao-tseu <adriencannet06@hotmail.fr>
2018-06-21 23:37:45 -0400

    Add start menu in the display, currently only supporting local game.

--- display.py
+++ display.py
@@ -23,6 +23,9 @@ class ChessDisplay():
 
         self.state = "MENU"
 
+        self.menuState = "START" # The menu has different states
+        self.menuSelection = "NONE" # The item currently being selected in the menu
+
         self.load_images()
 
         self.gameEngine = gameEngine
@@ -34,8 +37,10 @@ class ChessDisplay():
         self.check_positions = [[False for i in range(8)] for j in range(8)]
         self.checkmate_positions = [[False for i in range(8)] for j in range(8)]
 
-        self.currentMessage = ""
+        self.currentMessage = "Please select a game mode."
 
+        self.drawMenu()
+        self.updateMessage()
 
     def setTwoPlayersOnOneBoardMode(self):
         """ Sets the game engine on the two-players-on-one-board mode."""
@@ -154,6 +159,7 @@ class ChessDisplay():
         """ Updates the frame."""
         if self.state == "MENU":
             self.updateMenu()
+            self.updateMessage()
         elif self.state == "PLAYING":
             self.updateBoard()
             self.updateMessage()
@@ -163,9 +169,79 @@ class ChessDisplay():
             pass
 
     def updateMenu(self):
-        #TODO implement a menu
-        #for now directly switches to two-players-on-one-board mode.
-        self.setTwoPlayersOnOneBoardMode()
+        """
+        Updates the start menu.
+        """
+        if self.menuState == "START":
+            changeState = False
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.gameEngine.stop()
+
+                if event.type == pygame.MOUSEMOTION:
+                    mouse = event.pos
+                    if mouse[0] > self.width//2-250 and mouse[0] < self.width//2+250 and mouse[1] > 175 and mouse[1] < 325:
+                        if self.menuSelection != "LOCAL" and self.menuSelection != "LOCAL_DOWN":
+                            changeState = True
+                            self.menuSelection = "LOCAL"
+                    elif mouse[0] > self.width//2-250 and mouse[0] < self.width//2+250 and mouse[1] > 475 and mouse[1] < 625:
+                        if self.menuSelection != "ONLINE" and self.menuSelection != "ONLINE_DOWN":
+                            changeState = True
+                            self.menuSelection = "ONLINE"
+                    else:
+                        if self.menuSelection != "NONE":
+                            changeState = True
+                            self.menuSelection = "NONE"
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse = event.pos
+                    if mouse[0] > self.width//2-250 and mouse[0] < self.width//2+250 and mouse[1] > 175 and mouse[1] < 325:
+                        if self.menuSelection != "LOCAL_DOWN":
+                            changeState = True
+                            self.menuSelection = "LOCAL_DOWN"
+                    if mouse[0] > self.width//2-250 and mouse[0] < self.width//2+250 and mouse[1] > 475 and mouse[1] < 625:
+                        if self.menuSelection != "ONLINE_DOWN":
+                            changeState = True
+                            self.menuSelection = "ONLINE_DOWN"
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    mouse = event.pos
+                    if mouse[0] > self.width//2-250 and mouse[0] < self.width//2+250 and mouse[1] > 175 and mouse[1] < 325:
+                        if self.menuSelection == "LOCAL_DOWN":
+                            self.currentMessage = ""
+                            self.menuSelection = "NONE"
+                            self.setTwoPlayersOnOneBoardMode()
+                    if mouse[0] > self.width//2-250 and mouse[0] < self.width//2+250 and mouse[1] > 475 and mouse[1] < 625:
+                        if self.menuSelection == "ONLINE_DOWN":
+                            self.currentMessage = "The online mode is not supported yet."
+                            changeState = True
+                            self.menuSelection = "ONLINE"
+
+            if changeState:
+                fontTitle = pygame.font.Font("fonts/CFRemingtonTypewriter-Regul.ttf", 60)
+                if self.menuSelection == "LOCAL":
+                    pygame.draw.rect(self.screen, pygame.Color(170, 170, 170), [self.width//2-250, 175, 500, 150])
+                    local = fontTitle.render("Local game", True, (0, 0, 0))
+                    local_rect = local.get_rect(center=(self.width // 2, 250))
+                    self.screen.blit(local, local_rect)
+                elif self.menuSelection == "LOCAL_DOWN":
+                    pygame.draw.rect(self.screen, pygame.Color(140, 140, 140), [self.width//2-250, 175, 500, 150])
+                    local = fontTitle.render("Local game", True, (0, 0, 0))
+                    local_rect = local.get_rect(center=(self.width // 2, 250))
+                    self.screen.blit(local, local_rect)
+                elif self.menuSelection == "ONLINE":
+                    pygame.draw.rect(self.screen, pygame.Color(170, 170, 170), [self.width//2-250, 475, 500, 150])
+                    online = fontTitle.render("Online game", True, (0, 0, 0))
+                    online_rect = online.get_rect(center=(self.width // 2, 550))
+                    self.screen.blit(online, online_rect)
+                elif self.menuSelection == "ONLINE_DOWN":
+                    pygame.draw.rect(self.screen, pygame.Color(140, 140, 140), [self.width//2-250, 475, 500, 150])
+                    online = fontTitle.render("Online game", True, (0, 0, 0))
+                    online_rect = online.get_rect(center=(self.width // 2, 550))
+                    self.screen.blit(online, online_rect)
+                else:
+                    self.drawMenu()
+                pygame.display.flip()
 
     def updateBoard(self):
         """
@@ -202,17 +278,39 @@ class ChessDisplay():
         pygame.display.flip()
 
     def updateMessage(self):
-        font = pygame.font.SysFont("Lato Heavy", 30)
+        """
+        Change the currently displayed message using self.currentMessage
+        """
+        font = pygame.font.SysFont("Arial", 20)
         text = font.render(self.currentMessage, True, (0, 0, 0))
         text_rect = text.get_rect(center=(self.width // 2, (self.total_height+self.height+5) // 2))
         pygame.draw.rect(self.screen, pygame.Color(200, 200, 200), [
             0, self.height, self.width, self.total_height-self.height
         ])
-        pygame.draw.rect(self.screen, pygame.Color(0, 0, 0), [
-            0, self.height, self.width, 5
-        ])
+        pygame.draw.rect(self.screen, pygame.Color(0, 0, 0), [0, self.height, self.width, 5])
         self.screen.blit(text, text_rect)
-        # print(pygame.font.get_fonts())
+        pygame.display.flip()
+
+    def drawMenu(self):
+        """
+        Draws the start menu.
+        """
+        self.screen.blit(self.board, (0, 0))
+        fontTitle = pygame.font.Font("fonts/CFRemingtonTypewriter-Regul.ttf", 60)
+        if self.menuState == "START":
+            pygame.draw.rect(self.screen, pygame.Color(0, 0, 0), [self.width//2-255, 170, 510, 160])
+            pygame.draw.rect(self.screen, pygame.Color(200, 200, 200), [self.width//2-250, 175, 500, 150])
+            local = fontTitle.render("Local game", True, (0, 0, 0))
+            local_rect = local.get_rect(center=(self.width // 2, 250))
+            self.screen.blit(local, local_rect)
+
+            pygame.draw.rect(self.screen, pygame.Color(0, 0, 0), [self.width//2-255, 470, 510, 160])
+            pygame.draw.rect(self.screen, pygame.Color(200, 200, 200), [self.width//2-250, 475, 500, 150])
+            online = fontTitle.render("Online game", True, (0, 0, 0))
+            online_rect = online.get_rect(center=(self.width // 2, 550))
+            self.screen.blit(online, online_rect)
+        # TODO: implement the other states of the menu -> see with gameEngine
+        pygame.display.flip()
 
     def load_images(self):
         """Retrieve images from memory."""
